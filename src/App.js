45f63359a5eb14538f781b6ea774f62192f500d1
ZunainAliAzam <zunainazam456@gmail.com>
2024-01-16 01:41:46 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Footer from "./Components/Footer";
+import Work from "./Components/Work";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Work />
       <Footer />
     </div>
   );
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -24,8 +24,8 @@ const Work = () => {
   return (
     <div className="work-section-wrapper">
       <div className="work-section-top">
-        <p className="primary-subheading">Work</p>
-        <h1 className="primary-heading">How It Works</h1>
+        <h2 className="primary-heading">Work</h2>
+        <p className="primary-subheading">How It Works</p>
         <p className="primary-text">
           Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
           elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
