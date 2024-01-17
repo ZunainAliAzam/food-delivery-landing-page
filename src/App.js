aa531ea9b000b378b82aa5471003f85f0f123262
ZunainAliAzam <zunainazam456@gmail.com>
2024-01-17 23:58:42 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import Footer from "./Components/Footer";
 import Work from "./Components/Work";
+import About from "./Components/About";
+import Testimonial from "./Components/Testimonial";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
       <Work />
+      <Testimonial />
       <Footer />
     </div>
   );
